b5e2b2af2eac3b961a5be863825fe56160525488
Leandro Lavore <leolavore@gmail.com>
2019-12-14 22:32:15 +1000

    working with inlineStyles

--- src/accordion.js
+++ src/accordion.js
@@ -2,19 +2,16 @@ export class Accordion {
 
     continue(event){
         var targetCard = event.target.parentElement
-        var targetStyles = window.getComputedStyle(targetCard).top;
-        //var targetTop = targetStyles.getPropertyValue('top');
-        console.log(targetStyles)
-        //var cardToMove = targetCard.nextElementSibling
-        
-        
-        // var string = percentage.replace(/\%/g, "")
-        // var integer = parseInt(string, 10)
-        //debugger
-        
-        // cardToMove.style.top =  integer !== NaN 
-        //                             ? + 5 + '%' 
-        //                             : 5 + '%'
-    }
+        var cardToMove = targetCard.nextElementSibling
 
+        var percentageParent = targetCard.style.top
+        var stringParent = percentageParent.replace(/\%/g, "")
+        var integerParent = parseInt(stringParent, 10)
+
+        var percentageChild = cardToMove.style.top
+        var stringChild = percentageChild.replace(/\%/g, "")
+        var integerChild = parseInt(stringChild, 10)
+
+        cardToMove.style.top =  integerParent + 5 + '%'
+    }
 }
