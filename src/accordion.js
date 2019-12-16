7674c54484c173b0112f65b3b794336be94aee72
leandro <leandro@desygner.com>
2019-12-16 13:45:22 +1000

    started from scratch with flex

--- src/accordion.js
+++ src/accordion.js
@@ -1,20 +1,7 @@
 export class Accordion {
 
     continue(event){
-        var targetCard = event.target.parentElement
-        var targetStyles = window.getComputedStyle(targetCard).top;
-        //var targetTop = targetStyles.getPropertyValue('top');
-        console.log(targetStyles)
-        //var cardToMove = targetCard.nextElementSibling
-        
-        
-        // var string = percentage.replace(/\%/g, "")
-        // var integer = parseInt(string, 10)
-        //debugger
-        
-        // cardToMove.style.top =  integer !== NaN 
-        //                             ? + 5 + '%' 
-        //                             : 5 + '%'
+      
     }
 
 }
