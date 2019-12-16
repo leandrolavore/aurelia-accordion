4e2d96e347c78ade1d070367db70209eca9dad31
leandro <leandro@desygner.com>
2019-12-16 16:01:50 +1000

    added method to use Ids

--- src/accordion.js
+++ src/accordion.js
@@ -1,7 +1,27 @@
 export class Accordion {
 
-    continue(event){
-      
+    attached() {
+        document.querySelectorAll('.card').forEach((card, index) => {
+            card.setAttribute('id', index + 1)
+        })
+        document.querySelector('input').addEventListener('input', function () {
+            var previousElement = document.querySelector('.active')
+            previousElement
+                ? previousElement.classList.remove('active')
+                : null
+            document.getElementById(this.value).classList.add('active');
+        })
     }
 
+    continue(event) {
+        var parentDivId = event.target.parentElement.parentElement.id;
+        event.target.parentElement.parentElement.classList.remove('active')
+        document.getElementById(parseInt(parentDivId) + 1).classList.add('active');
+    }
+    
+    previous(event) {
+        var parentDivId = event.target.parentElement.parentElement.id;
+        event.target.parentElement.parentElement.classList.remove('active')
+        document.getElementById(parseInt(parentDivId) - 1).classList.add('active');
+    }
 }
